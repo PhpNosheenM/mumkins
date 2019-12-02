efd8ccbc4bc3c4dbb9ae2851e2896ad7eebc4614
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2019-12-02 10:36:01 +0530

    Category

--- src/Controller/Api/CategoriesController.php
+++ src/Controller/Api/CategoriesController.php
@@ -12,8 +12,29 @@ use App\Controller\Api\AppController;
 class CategoriesController extends AppController
 {
    public function categoryitem(){
-	   
+	    $sort="ASC";
 	    $category_id=$this->request->query('category_id');
+		$sub_category_id= @$this->request->query('sub_category_id');
+		$price= @$this->request->query('price');
+		$color= @$this->request->query('color');
+		$size= @$this->request->query('size');
+		$sort= @$this->request->query('sort');
+		$conditions=[];
+			if(!empty($size)){
+				 $conditions[]=['ItemRows.size_id'=>$size];
+			   }
+			 if(!empty($color)){
+				 $conditions[]=['ItemRows.color_id'=>$color];
+			   }
+			   
+			 if(!empty($price)){
+					$prices=explode('-',$price);
+					 $conditions[]=['ItemRows.sale_rate >= '=>$prices[0]];
+					 $conditions[]=['ItemRows.sale_rate <= '=>$prices[1]];
+				};
+			
+			//pr($conditions); exit;
+			 
 		if(!empty($category_id)){
 			
 			$Categories=$this->Categories->get($category_id,['contain'=>['ChildCategories']]);
@@ -27,7 +48,23 @@ class CategoriesController extends AppController
 			$FooterSections=$this->Categories->FooterSections->find()->where(['FooterSections.is_deleted'=>0,'show_on'=>'Category Page']);
 			
 			$Itemlist=$this->Categories->Items->find()->where(['Items.category_id IN'=>$subcategory_id]);
-			$Itemlist->contain(['ItemRows'=>['Colors','Sizes','ItemImages']]);
+			//$Itemlist->contain(['ItemRows'=>['Colors','Sizes','ItemImages']]);
+			$Itemlist->contain(['ItemRows'=>function($q) use($conditions){
+				return $q->where($conditions)->contain(['Colors','Sizes','ItemImages']);
+			}]);
+			
+			if(!empty($sub_category_id)){
+				$Itemlist->where(['Items.category_id'=>$sub_category_id]);
+				
+			}
+			
+			if(!empty($sort)){
+				$Itemlist->order(['Items.name'=>$sort]);
+				
+			}
+			
+			 // echo $size;
+			   
 			$success = true;
 			$message = 'Data Found';
 			
